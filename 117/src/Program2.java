d31aff440fc4b53124a617ffa1b4a281b0fa2455
jayasuriya <jayasuriya@LAPTOP-OEOJ4AAD>
2020-06-12 22:07:34 +0530

    commit

--- 117/src/Program2.java
+++ 117/src/Program2.java
@@ -2,7 +2,7 @@
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome "+args[0]);
 
 	}
 
---
+++ 117/src/Program3.java
@@ -0,0 +1,10 @@
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+		System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+n);
+
+	}
+
+}
