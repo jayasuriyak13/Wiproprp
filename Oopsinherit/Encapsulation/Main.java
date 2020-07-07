f44757816110f5955ae378349435dd4f37d30dd0
jayasuriyak13 <60653999+jayasuriyak13@users.noreply.github.com>
2020-07-07 17:48:11 +0530

    Add files via upload

---
+++ Oopsinherit/Encapsulation/Main.java
@@ -0,0 +1,13 @@
+package Encapsulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Author a=new Author("William Shakespeare","shakespeare@gmail.com",'M');
+		Book b=new Book("William Shakespeare","shakespeare@gmail.com",'M',"Hamlet", 450,3);
+		System.out.println(b);
+		//System.out.println(a);
+	}
+	
+
+}
