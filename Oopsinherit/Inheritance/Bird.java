47159b5cb6e107b2c32935919be598ad780c9298
jayasuriyak13 <60653999+jayasuriyak13@users.noreply.github.com>
2020-07-07 17:49:09 +0530

    Add files via upload

---
+++ Oopsinherit/Inheritance/Bird.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+public class Bird extends Animal {
+	public void eat() {
+		System.out.println("Bird eat");
+	}
+	public void sleep() {
+		System.out.println("Bird sleep");
+	}
+	public void fly() {
+		System.out.println("Bird Fly");
+	}
+	
+
+}
